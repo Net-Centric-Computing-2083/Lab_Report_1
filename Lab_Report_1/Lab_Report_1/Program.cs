c0d0114b5cceecea6bdfae46870a5c838e5a63f9
Shreebisha Shrestha <shreebishacsit23@oic.edu.np>
2026-07-08 16:32:22 +0545

    SMS-lab1

---
+++ Lab_Report_1/Lab_Report_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Marks { get; set; }
+
+    public Student(int id, string name, double marks)
+    {
+        Id = id;
+        Name = name;
+        Marks = marks;
+    }
+
+    public string GetResult()
+    {
+        if (Marks >= 40)
+            return "Passed";
+        else
+            return "Failed";
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine("\n--- Student Information ---");
+        Console.WriteLine("Student ID: " + Id);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Marks: " + Marks);
+        Console.WriteLine("Result: " + GetResult());
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Student ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Student Marks: ");
+        double marks = Convert.ToDouble(Console.ReadLine());
+
+        Student student = new Student(id, name, marks);
+
+        student.DisplayInfo();
+
+        Console.ReadLine();
+    }
+}
